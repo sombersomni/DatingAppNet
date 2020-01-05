800039668543407a75e076fbfc50850bb4ab8ed5
Xavier Palin <xpalin@tuyatech.com>
2020-01-05 03:17:15 -0600

    Added validation to UserForRegisterDto

--- DatingApp.API/DTOs/UserForRegisterDto.cs
+++ DatingApp.API/DTOs/UserForRegisterDto.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.DTOs
 {
     public class UserForRegisterDto
     {
+        [Required]
+        [MaxLength(255)]
         public string UserName { get; set; }
+        [Required]
+        [MinLength(4, ErrorMessage = "You must specify a password greater than Length 4")]
         public string Password { get; set; }
     }
 }
